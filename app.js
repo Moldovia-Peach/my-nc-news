13c11b7d29fc50a7e5f7f651ffb53d38c9455de0
Moldovia-Peach <hughes.mollyann@outlook.com>
2024-12-16 09:38:22 +0000

    updated cors

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const {
   handlePsqlErrors,
   handleUnmatchedEndpoints,
 } = require("./errors/errorHandling");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
